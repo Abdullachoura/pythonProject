5325a7c8840427eadbc600c29b07d7e0e49bba39
thorax2077 <thomasrack1972@gmail.com>
2023-02-22 15:34:06 +0100

    funktion class, skallierung überschrieben

---
+++ function.py
@@ -0,0 +1,58 @@
+import numpy
+
+
+class Function:
+
+    original: []
+    deriv_1: []
+    deriv_2: []
+    deriv_3: []
+
+    def __init__(self, *args):
+        self.original = [args]
+        for i in range(len(self.original)):
+            if i == 0: continue
+            self.deriv_1= i * self.original[i]
+        for i in range(len(self.deriv_1)):
+            if i == 0: continue
+            self.deriv_2 = i * self.deriv_1[i]
+        for i in range(len(self.deriv_2)):
+            if i == 0: continue
+            self.deriv_3 = i * self.deriv_2[i]
+
+    def calc_original(self, x):
+        to_return = 0
+        for i in range(len(self.original)):
+            to_return += self.original[i]* x **i
+        return to_return
+    def calc_deriv1(self, x):
+        to_return = 0
+        for i in range(len(self.deriv_1)):
+            to_return += self.deriv_1[i] * x **i
+        return to_return
+
+    def calc_deriv2(self, x):
+        to_return = 0
+        for i in range(len(self.deriv_2)):
+            to_return += self.deriv_2[i] * x **i
+        return to_return
+
+    def calc_deriv3(self, x):
+        to_return = 0
+        for i in range(len(self.deriv_3)):
+            to_return = self.deriv_3 * x **i
+        return to_return
+
+    def calc_for_range(self, fun, min, max, inc):
+        '''
+        gibt ergebnisse als array zurück
+        :param fun: calc funktion von classe Funktion
+        :param min: untere grenze\n
+        :param max: obere grenze\n
+        :param inc: schritt größe\n
+        :return: array der ergebnisse
+        '''
+        to_return_arr = []
+        for i in range(min,max, inc):
+            to_return_arr.append(fun(i))
+        return to_return_arr
--- graphWindow.py
+++ graphWindow.py
@@ -1,30 +1,34 @@
 import tkinter
 import tkinter as tk
+from logging import exception
+
+import function as fun
 
 from matplotlib.backends.backend_tkagg import (
-FigureCanvasTkAgg, NavigationToolbar2Tk
+    FigureCanvasTkAgg, NavigationToolbar2Tk
 )
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import numpy as np
 
-class GraphWindow:
 
+class GraphWindow:
     functionList: []
     scaleVar = tk.StringVar
-
-
+    scale: float
+    offset_x: float
+    offset_y: float
 
     def __init__(self):
-
-
+        self.scale = 10
+        self.offset_x = 0
+        self.offset_y = 0
         self.functionList = []
         root = tk.Tk()
         self.scaleVar = tk.StringVar()
         self.scaleVar.set("10")
 
         root.title('Functional')
-
 
         menubar = tk.Menu(root)
         root.config(menu=menubar)
@@ -37,69 +41,96 @@ class GraphWindow:
             menu=funktion_menu
         )
 
-        self.fig, self.ax= plt.subplots(1,1)
+        self.fig, self.ax = plt.subplots(1, 1)
         self.ax.grid(True)
-        self.ax.set_xlim(-10,10)
-        self.ax.set_ylim(-10,10)
+        self.ax.set_xlim(-1 * self.scale + self.offset_x, self.scale + self.offset_x)
+        self.ax.set_ylim(-1 * self.scale + self.offset_y, self.scale + self.offset_y)
         self.ax.set_aspect('equal')
 
         self.canvas = FigureCanvasTkAgg(self.fig, master=root)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(fill='both', expand=1)
 
-        btn_increment_scale = tk.Button(text='+', command=self.increment_scale)
-        btn_increment_scale.pack(side="bottom")
+        frame_scale = tk.Frame(master=root)
+        frame_scale.pack(side="bottom", expand='x')
 
-        btn_decrement_scale = tk.Button(text='-', command=self.decrement_scale)
-        btn_decrement_scale.pack(side="bottom")
-
-        ent_scale = tk.Entry(textvariable=self.scaleVar)
+        ent_scale = tk.Entry(frame_scale, textvariable=self.scaleVar)
         ent_scale.bind("<Return>", func=self.enter_scale)
-        ent_scale.pack(side="bottom")
+        ent_scale.pack(side="left")
 
-        #listboxFunctions = tkinter.Listbox(root, listvariable=self.functionList, width=50)
-        #listboxFunctions.pack(side="left", fill='y')
+        btn_increment_scale = tk.Button(frame_scale, text='+', command=self.increment_scale)
+        btn_increment_scale.pack(side="left")
+
+        btn_decrement_scale = tk.Button(frame_scale, text='-', command=self.decrement_scale)
+        btn_decrement_scale.pack(side="left")
+
+        # listboxFunctions = tkinter.Listbox(root, listvariable=self.functionList, width=50)
+        # listboxFunctions.pack(side="left", fill='y')
 
         root.mainloop()
 
+    def draw(self):
+        self.ax.set_xlim(-1 * self.scale + self.offset_x, self.scale + self.offset_x)
+        self.ax.set_ylim(-1 * self.scale + self.offset_y, self.scale + self.offset_y)
+        self.canvas.draw()
+
     def increment_scale(self):
-        print("increment")
         try:
-            scale = float(self.scaleVar.get())
-            scale += 10
-            if scale == 0:
-                scale = 10
-            self.ax.set_xlim(-1 * scale, scale)
-            self.ax.set_ylim(-1 * scale, scale)
-            self.canvas.draw()
-            self.scaleVar.set(str(scale))
+            self.scale = float(self.scaleVar.get())
+            self.scale += 10
+            if self.scale <= 0:
+                self.scale = 10
+            self.draw()
+            self.scaleVar.set(str(self.scale))
         except:
             print("exception")
-
 
     def decrement_scale(self):
-        print("decrement")
         try:
-            scale = float(self.scaleVar.get())
-            scale -= 10
-            if scale==0:
-                scale = -10
-            self.ax.set_xlim(-1 * scale, scale)
-            self.ax.set_ylim(-1 * scale, scale)
-            self.canvas.draw()
-            self.scaleVar.set(str(scale))
+            self.scale = float(self.scaleVar.get())
+            self.scale -= 10
+            if self.scale <= 0:
+                self.scale = 10
+            self.draw()
+            self.scaleVar.set(str(self.scale))
         except:
             print("exception")
-
 
     def enter_scale(self, e):
         try:
-            scale = float(self.scaleVar.get())
-            if scale == 0:
-                scale = 10
-            self.ax.set_xlim(-1 * scale, scale)
-            self.ax.set_ylim(-1 * scale, scale)
-            self.canvas.draw()
-            self.scaleVar.set(str(scale))
+            self.scale = float(self.scaleVar.get())
+            if self.scale == 0:
+                self.scale = 10
+            self.draw()
+            self.scaleVar.set(str(self.scale))
         except:
             print("exception")
+
+
+    def convert_factor_entry(self, factor: str) -> float:
+        if factor[0] == '+':
+            to_return = float(factor[1:])
+            return to_return
+        elif factor[0] == '-':
+            to_return = -1 * float(factor[1:])
+            return to_return
+        else:
+            raise ValueError("kein valides Vorzeichen")
+
+    def linear_function(self):
+        lin = tk.Tk()
+        lin_factor = tk.StringVar()
+        lin_const = tk.StringVar()
+
+        entry_lin_factor = tk.Entry(lin, textvariable=lin_factor)
+        entry_lin_factor.grid(column=0, row=1)
+
+        label_lin_var = tk.Label(lin, text="x")
+        label_lin_var.grid(column=1, row=1)
+
+        entry_lin_const = tk.Entry(lin, textvariable=lin_const)
+        entry_lin_const.grid(column=2, row=1)
+
+        factor_0 =
+
+        button_lin = tk.Button("eingabe", command= self.functionList.append(fun.Function()))
